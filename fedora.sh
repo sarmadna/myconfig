cd7ffc2e611ae53e9ec2f34e9e4068eae298e2de
Sarmad <sarmad@localhost.localdomain>
2021-04-08 14:17:53 +0300

    scripts for ubuntu and fedora

---
+++ fedora.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+echo
+
+sudo dnf -y install wget curl vim rofi zsh gnome-tweaks gnome-shell-extension-dash-to-dock powerline-fonts util-linux
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp ~/Downloads/MyLinux/dotfiles/vimrc ~/.vimrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+cp ~/Downloads/MyLinux/dotfiles/zshrc ~/.zshrc
+
+cp ~/Downloads/MyLinux/omztheme/saros.zsh-theme ~/.oh-my-zsh/themes/
+
+chsh -s $(which zsh)
+
---
+++ ubuntu.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+echo
+
+sudo apt -y install wget curl vim rofi gnome-session gnome-shell-extensions font-manager papirus-icon-theme libreoffice vlc fonts-powerline zsh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp ~/Downloads/MyLinux/dotfiles/vimrc ~/.vimrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+cp ~/Downloads/MyLinux/dotfiles/zshrc ~/.zshrc
+
+cp ~/Downloads/MyLinux/omztheme/saros.zsh-theme ~/.oh-my-zsh/themes/
+
+chsh -s $(which zsh)
+
