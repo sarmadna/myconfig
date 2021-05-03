f92f9d27ec47740ba4d007e7d21c8afeebc937e3
Sarmad <sarmad_n_a@hotmail.com>
2021-05-03 12:04:23 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -6,27 +6,25 @@ cyan='\033[1;36m'
 
 nc='\033[0m'
 
-printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
 
 sudo dnf -y install wget curl vim rofi zsh gnome-tweaks util-linux-user papirus-icon-theme gnome-extensions-app
 
 # sudo dnf -y install arc-theme plank plank-docklets arc-theme-plank
 
-printf "${yellow}>>>${nc} ${cyan}Applying dark theme...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 
-printf "${yellow}>>>${nc} ${cyan}Changing icons theme...${nc}"
-
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
 
-printf "${yellow}>>>${nc} ${cyan}Setting desktop and lockscreen background...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Setting desktop and lockscreen background...${nc}\n"
 
 gsettings set org.gnome.desktop.background picture-uri file:///home/sarmad/Downloads/MyLinux/bg/fedora.jpg
 
 gsettings set org.gnome.desktop.screensaver picture-uri file:///home/sarmad/Downloads/MyLinux/bg/fedora.jpg
 
-printf "${yellow}>>>${nc} ${cyan}Installing Vundle.vim...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Installing Vundle.vim...${nc}\n"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -34,13 +32,13 @@ cp ~/Downloads/MyLinux/dotfiles/vimrc ~/.vimrc
 
 vim +PluginInstall +qall
 
-printf "${yellow}>>>${nc} ${cyan}Installing powerline fonts...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Installing powerline fonts...${nc}\n"
 
 git clone https://github.com/powerline/fonts.git ~/Downloads/PowerlineFonts
 
 sh ~/Downloads/PowerlineFonts/install.sh
 
-printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}"
+printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
