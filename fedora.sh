489944b62b9345f803278b7d701ad17d286e7baf
sarmadna <sarmad_n_a@hotmail.com>
2021-05-04 09:39:06 +0300

    minor changes

--- fedora.sh
+++ fedora.sh
@@ -21,16 +21,16 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ara'), ('xkb', 'iq+ku_ara')]"
 
-printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ~/Downloads/MyLinux/dotfiles/vimrc ~/.vimrc
-vim +PluginInstall +qall
-sed -i '16,17s/^.//' ~/.vimrc
-
 printf "${yellow}>>>${nc} ${cyan}Installing powerline fonts...${nc}\n"
 git clone https://github.com/powerline/fonts.git ~/Downloads/PowerlineFonts
 sh ~/Downloads/PowerlineFonts/install.sh
 #gsettings set org.gnome.desktop.interface monospace-font-name "DejaVu Sans Mono for Powerline Book 11"
+
+printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ~/Downloads/MyLinux/dotfiles/vimrc ~/.vimrc
+vim +PluginInstall +qall
+#sed -i '16,17s/^.//' ~/.vimrc
 
 printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
