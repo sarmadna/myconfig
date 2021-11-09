66bdc594927a972867d6bd942b4848cd73eda3b8
Sarmad <sarmad_n_a@hotmail.com>
2021-11-09 17:55:10 +0300

    Update exturls.sh

--- fedora/exturls.sh
+++ fedora/exturls.sh
@@ -1,14 +1,8 @@
 #! /bin/sh
 
-xdg-open https://extensions.gnome.org/extension/3628/arcmenu/
-xdg-open https://extensions.gnome.org/extension/1160/dash-to-panel/
-xdg-open https://extensions.gnome.org/extension/2087/desktop-icons-ng-ding/
-xdg-open https://extensions.gnome.org/extension/4135/espresso/
-xdg-open https://extensions.gnome.org/extension/2848/hide-keyboard-layout/
+xdg-open https://extensions.gnome.org/extension/307/dash-to-dock/
 xdg-open https://extensions.gnome.org/extension/36/lock-keys/
 xdg-open https://extensions.gnome.org/extension/708/panel-osd/
 xdg-open https://extensions.gnome.org/extension/7/removable-drive-menu/
-xdg-open https://extensions.gnome.org/extension/3733/tiling-assistant/
-xdg-open https://extensions.gnome.org/extension/2890/tray-icons-reloaded/
-xdg-open https://extensions.gnome.org/extension/4099/no-overview/
-
+xdg-open https://extensions.gnome.org/extension/517/caffeine/
+xdg-open https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
