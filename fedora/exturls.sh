038948f563c6a5902f2f46d12cc2528cc5192e29
Sarmad <sarmad_n_a@hotmail.com>
2021-11-09 18:02:51 +0300

    Delete exturls.sh

--- fedora/exturls.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-xdg-open https://extensions.gnome.org/extension/307/dash-to-dock/
-xdg-open https://extensions.gnome.org/extension/36/lock-keys/
-xdg-open https://extensions.gnome.org/extension/708/panel-osd/
-xdg-open https://extensions.gnome.org/extension/7/removable-drive-menu/
-xdg-open https://extensions.gnome.org/extension/517/caffeine/
-xdg-open https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
