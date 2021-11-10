927e82b115cb444a61377cdf5b2a6b2a7498a5b3
Sarmad <sarmad_n_a@hotmail.com>
2021-11-10 13:44:00 -0500

    Create kde.sh

---
+++ fedora/kde.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+yellow='\033[1;33m'
+cyan='\033[1;36m'
+nc='\033[0m'
+
+printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
+sudo timedatectl set-timezone Asia/Baghdad
+sudo dnf -y install wget curl vim zsh htop util-linux-user cmatrix neofetch https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
+git clone https://github.com/vinceliuice/Qogir-theme.git $HOME/Downloads/Qogir-theme
+sudo sh $HOME/Downloads/Qogir-theme/install.sh -d /usr/share/themes -l fedora
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git $HOME/Downloads/Qogir-icon-theme
+sh $HOME/Downloads/Qogir-icon-theme/install.sh
+
+printf "${yellow}>>>${nc} ${cyan}Installing powerline fonts...${nc}\n"
+git clone https://github.com/powerline/fonts.git $HOME/Downloads/PowerlineFonts
+sh $HOME/Downloads/PowerlineFonts/install.sh
+sudo cp $HOME/Downloads/PowerlineFonts/Terminus/PSF/*.gz /usr/lib/kbd/consolefonts/
+sudo mv /etc/vconsole.conf /etc/vconsole.conf.old
+sudo cp $HOME/Downloads/MyLinux/dotfiles/vconsole.conf /etc/vconsole.conf
+
+printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp $HOME/Downloads/MyLinux/dotfiles/vimrc $HOME/.vimrc
+vim +PluginInstall +qall
+cp $HOME/Downloads/MyLinux/dotfiles/tmux.conf $HOME/.tmux.conf
+
+printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+cp $HOME/Downloads/MyLinux/dotfiles/zshrc $HOME/.zshrc
+cp $HOME/Downloads/MyLinux/omztheme/saros.zsh-theme $HOME/.oh-my-zsh/themes/
+git clone https://github.com/joepvd/tty-solarized $HOME/.oh-my-zsh/custom/plugins/tty-solarized
+chsh -s $(which zsh)
