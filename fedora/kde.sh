73126468aad7d643f2bd0b5e8797b2e3e8f2dbe7
Sarmad <sarmad_n_a@hotmail.com>
2022-01-29 14:45:00 +0300

    Update kde.sh

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -6,7 +6,7 @@ nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
 sudo timedatectl set-timezone Asia/Baghdad
-sudo dnf -y install wget curl vim zsh htop util-linux-user cmatrix neofetch cava fedora-workstation-repositories
+sudo dnf -y install wget curl vim zsh htop latte-dock util-linux-user cmatrix neofetch cava fedora-workstation-repositories
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --set-enabled google-chrome
