891b803d4d36e5f6ec06dab75636606ec589eaf4
Sarmad <sarmad_n_a@hotmail.com>
2022-02-04 21:21:16 +0300

    Update kde.sh

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -10,7 +10,6 @@ sudo dnf -y install wget curl vim zsh htop latte-dock util-linux-user cmatrix ne
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --set-enabled google-chrome
-sudo dnf install google-chrome-stable telegram-desktop
 
 #printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
 #git clone https://github.com/vinceliuice/Qogir-icon-theme.git $HOME/Downloads/Qogir-icon-theme
