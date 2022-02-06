e682c9dceeb60824dc0ee24ace1d637f7ce6c35e
Sarmad <sarmad_n_a@hotmail.com>
2022-02-06 23:00:54 +0300

    Update kde.sh

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -6,17 +6,14 @@ nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
 sudo timedatectl set-timezone Asia/Baghdad
-sudo dnf -y install wget curl vim zsh htop latte-dock util-linux-user cmatrix neofetch cava fedora-workstation-repositories
+sudo dnf -y install wget curl vim zsh tmux htop latte-dock util-linux-user cmatrix neofetch cava fedora-workstation-repositories
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --set-enabled google-chrome
 
-#printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
-#git clone https://github.com/vinceliuice/Qogir-icon-theme.git $HOME/Downloads/Qogir-icon-theme
-#sh $HOME/Downloads/Qogir-icon-theme/install.sh
-#mkdir -p $HOME/.local/share/plasma/plasmoids
-#git clone https://github.com/vinceliuice/Qogir-kde.git $HOME/Downloads/Qogir-kde
-#sh $HOME/Downloads/Qogir-kde/install.sh
+printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git $HOME/Downloads/Qogir-icon-theme
+sh $HOME/Downloads/Qogir-icon-theme/install.sh
 
 printf "${yellow}>>>${nc} ${cyan}Installing fonts...${nc}\n"
 git clone https://github.com/powerline/fonts.git $HOME/Downloads/PowerlineFonts
