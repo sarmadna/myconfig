1293e481e4fc2185d33422f14e00bc26e09e02f7
Sarmad <sarmad_n_a@hotmail.com>
2022-03-07 22:32:02 +0300

    Update and rename base.sh to kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -10,10 +10,13 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
-pacman -S grub efibootmgr networkmanager firewalld terminus-font xorg base-devel dialog gvfs mtools
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant firewalld terminus-font xorg gvfs mtools bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack xdg-user-dirs xdg-utils xorg plasma plasma-wayland-session kde-applications sddm
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable firewalld
+systemctl enable sddm
+systemctl enable cups
+systemctl enable bluetooth
