bc24326bf3b91f40c8a4edcde584f793f0575b78
Sarmad <sarmad_n_a@hotmail.com>
2022-03-08 00:51:25 +0300

    Update kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -6,6 +6,7 @@ nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
 sudo pacman -S xorg plasma plasma-wayland-session firefox kde-applications papirus-icon-theme
+sudo systemctl enable sddm
 
 printf "${yellow}>>>${nc} ${cyan}Installing fonts...${nc}\n"
 git clone https://github.com/powerline/fonts.git $HOME/Downloads/PowerlineFonts
