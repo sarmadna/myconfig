72c8aa0a351b9eb959e2e31e091ef80f9918f1d4
Sarmad <sarmad_n_a@hotmail.com>
2022-03-07 10:08:53 +0300

    Update and rename kde.sh to base.sh

--- arch/base.sh
+++ arch/base.sh
@@ -2,19 +2,18 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "archvm" >> /etc/hostname
+echo "archlinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 archvm.localdomain archvm" >> /etc/hosts
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
-pacman -S grub efibootmgr networkmanager firewalld terminus-font xorg plasma plasma-wayland-session
+pacman -S grub efibootmgr networkmanager firewalld terminus-font xorg base-devel dialog gvfs mtools
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable firewalld
-systemctl enable sddm.service
