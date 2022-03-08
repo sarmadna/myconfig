154df8cc2a76902736305772104f57738dbbe85c
Sarmad <sarmad_n_a@hotmail.com>
2022-03-08 08:31:31 +0300

    Update base_mbp.sh

--- arch/base_mbp.sh
+++ arch/base_mbp.sh
@@ -16,12 +16,13 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 macbook.localdomain macbook" >> /etc/hosts
 printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
-pacman -S grub efibootmgr networkmanager network-manager-applet wpa_supplicant broadcom-wl dialog firewalld xdg-user-dirs xdg-utils terminus-font gvfs mtools base-devel ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+pacman -S grub efibootmgr networkmanager network-manager-applet wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs mtools base-devel ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 printf "${yellow}>>>${nc} ${cyan}Installing GRUB...${nc}\n"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 printf "${yellow}>>>${nc} ${cyan}Enabling services...${nc}\n"
 systemctl enable NetworkManager
+systemctl enable bluetooth
 systemctl enable firewalld
 systemctl enable cups.service
 systemctl enable tlp
