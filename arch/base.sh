25bc5b3932d3546f42cf813411a6be223abed18f
Sarmad <sarmad_n_a@hotmail.com>
2022-03-10 12:50:32 +0300

    Update and rename base_mbp.sh to base.sh

--- arch/base.sh
+++ arch/base.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 yellow='\033[1;33m'
 cyan='\033[1;36m'
 red='\033[1;31m'
 nc='\033[0m'
+
 printf "${yellow}>>>${nc} ${cyan}Starting installation script...${nc}\n"
+
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
 hwclock --systohc
 #sed -i '178s/.//' /etc/locale.gen
@@ -14,18 +17,35 @@ echo "macbook" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 macbook.localdomain macbook" >> /etc/hosts
+
 printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
-pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+
+# Uncomment for specific system
+
+# MacBook Pro
+#pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+
+# Lenovo ThinkPad x230
+#pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+
+# Desktop PC (AMD+NVIDIA)
+#pacman -S grub efibootmgr os-prober networkmanager network-manager-applet dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+
 printf "${yellow}>>>${nc} ${cyan}Installing GRUB...${nc}\n"
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
 printf "${yellow}>>>${nc} ${cyan}Enabling services...${nc}\n"
+
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable firewalld
 systemctl enable cups.service
 systemctl enable tlp
+
 printf "${yellow}>>>${nc} ${cyan}Creating user...${nc}\n"
+
 useradd -m sarmad
 usermod -aG wheel sarmad
 printf "${red}Please change USER password${nc}\n"
