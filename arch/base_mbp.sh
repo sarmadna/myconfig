7339540e41094e6d8e179fe0019d9b885411d2f6
Sarmad <sarmad_n_a@hotmail.com>
2022-03-10 12:15:45 +0300

    Update base_mbp.sh

--- arch/base_mbp.sh
+++ arch/base_mbp.sh
@@ -15,7 +15,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 macbook.localdomain macbook" >> /etc/hosts
 printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
-pacman -S grub efibootmgr networkmanager network-manager-applet wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs mtools base-devel ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 printf "${yellow}>>>${nc} ${cyan}Installing GRUB...${nc}\n"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -28,8 +28,8 @@ systemctl enable tlp
 printf "${yellow}>>>${nc} ${cyan}Creating user...${nc}\n"
 useradd -m sarmad
 usermod -aG wheel sarmad
-printf "${red}PLEASE CHANGE USER PASSWORD${nc}\n"
+printf "${red}Please change USER password${nc}\n"
 passwd sarmad
-printf "${red}PLEASE CHANGE ROOT PASSWORD${nc}\n"
+printf "${red}Please change ROOT password${nc}\n"
 passwd
 EDITOR=vim visudo
