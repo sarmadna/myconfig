ea2adb79ed586f08cc33d4cc49fdec499ee29fbc
Sarmad <sarmad_n_a@hotmail.com>
2022-03-10 12:55:04 +0300

    Update base.sh

--- arch/base.sh
+++ arch/base.sh
@@ -6,7 +6,6 @@ red='\033[1;31m'
 nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Starting installation script...${nc}\n"
-
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
 hwclock --systohc
 #sed -i '178s/.//' /etc/locale.gen
@@ -19,9 +18,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 macbook.localdomain macbook" >> /etc/hosts
 
 printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
-
 # Uncomment for specific system
-
 # MacBook Pro
 #pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 
@@ -32,12 +29,10 @@ printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
 #pacman -S grub efibootmgr os-prober networkmanager network-manager-applet dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 
 printf "${yellow}>>>${nc} ${cyan}Installing GRUB...${nc}\n"
-
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 printf "${yellow}>>>${nc} ${cyan}Enabling services...${nc}\n"
-
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable firewalld
@@ -45,7 +40,6 @@ systemctl enable cups.service
 systemctl enable tlp
 
 printf "${yellow}>>>${nc} ${cyan}Creating user...${nc}\n"
-
 useradd -m sarmad
 usermod -aG wheel sarmad
 printf "${red}Please change USER password${nc}\n"
