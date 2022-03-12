ab681d7257f56887a5fd94c88da1448d4b07c233
Sarmad <sarmad_n_a@hotmail.com>
2022-03-12 09:15:50 +0300

    Update base.sh

--- arch/base.sh
+++ arch/base.sh
@@ -8,8 +8,8 @@ nc='\033[0m'
 printf "${yellow}>>>${nc} ${cyan}Starting installation script...${nc}\n"
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
 hwclock --systohc
-#sed -i '178s/.//' /etc/locale.gen
-vim /etc/locale.gen
+sed -i '178s/.//' /etc/locale.gen
+#vim /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "macbook" >> /etc/hostname
@@ -20,13 +20,13 @@ echo "127.0.1.1 macbook.localdomain macbook" >> /etc/hosts
 printf "${yellow}>>>${nc} ${cyan}Installing packages...${nc}\n"
 # Uncomment for specific system
 # MacBook Pro
-#pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+#pacman -S --no-confirm grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant broadcom-wl dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 
 # Lenovo ThinkPad x230
-#pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+#pacman -S --no-confirm grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 
 # Desktop PC (AMD+NVIDIA)
-#pacman -S grub efibootmgr os-prober networkmanager network-manager-applet dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
+#pacman -S --no-confirm grub efibootmgr os-prober networkmanager network-manager-applet dialog bluez bluez-utils firewalld xdg-user-dirs xdg-utils terminus-font gvfs gvfs-afc mtools dosfstools base-devel linux-headers ntfs-3g cups hplip zsh tmux tlp cmatrix neofetch htop
 
 printf "${yellow}>>>${nc} ${cyan}Installing GRUB...${nc}\n"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
