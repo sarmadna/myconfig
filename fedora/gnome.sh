4967ff4a7a21ad29e9d3eae74666c474f29947f3
sarmadna <sarmad_n_a@hotmail.com>
2022-04-16 12:32:15 +0300

    update

--- fedora/gnome.sh
+++ fedora/gnome.sh
@@ -59,5 +59,6 @@ printf "${yellow}>>>${nc} ${cyan}Loading gnome-shell extensions page...${nc}\n"
 xdg-open https://extensions.gnome.org/extension/307/dash-to-dock/
 xdg-open https://extensions.gnome.org/extension/36/lock-keys/
 xdg-open https://extensions.gnome.org/extension/517/caffeine/
-xdg-open https://extensions.gnome.org/extension/4099/no-overview/
 xdg-open https://extensions.gnome.org/extension/19/user-themes/
+xdg-open https://extensions.gnome.org/extension/7/removable-drive-menu/
+xdg-open https://extensions.gnome.org/extension/615/appindicator-support/
