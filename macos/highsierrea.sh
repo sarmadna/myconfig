3406df196abdcf0e6082dad8c3669ccf4c5eec1c
sarmadna <sarmad_n_a@hotmail.com>
2022-05-01 10:15:48 +0300

    update

--- macos/highsierrea.sh
+++ macos/highsierrea.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+yellow='\033[1;33m'
+cyan='\033[1;36m'
+nc='\033[0m'
+
 printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 cp $HOME/Downloads/MyLinux/dotfiles/vimrc $HOME/.vimrc
