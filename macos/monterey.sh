09be6ebfd32d638af3e9eb365215dc14c3ee5d00
sarmadna <sarmad_n_a@hotmail.com>
2022-05-01 10:10:26 +0300

    update

---
+++ macos/monterey.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp $HOME/Downloads/MyLinux/dotfiles/vimrc $HOME/.vimrc
+vim +PluginInstall +qall
+cp $HOME/Downloads/MyLinux/dotfiles/tmux.conf $HOME/.tmux.conf
+
+printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp $HOME/Downloads/MyLinux/dotfiles/zshrc $HOME/.zshrc
+cp $HOME/Downloads/MyLinux/omztheme/saros.zsh-theme $HOME/.oh-my-zsh/themes/
+
