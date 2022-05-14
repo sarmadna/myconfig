7f196bc86e7103458ce8caf322ffd29315e7ee7f
Sarmad <sarmad_n_a@hotmail.com>
2022-05-14 18:29:15 +0300

    Update kde.sh

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -6,14 +6,12 @@ nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Starting post-installation script...${nc}\n"
 sudo timedatectl set-timezone Asia/Baghdad
-sudo dnf -y install wget curl vim zsh tmux htop latte-dock util-linux-user cmatrix neofetch cava fedora-workstation-repositories
-sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install wget curl vim zsh tmux htop latte-dock util-linux-user cmatrix neofetch cava fedora-workstation-repositories \
+https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupinstall -y "KDE Plasma Workspaces"
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf -y install google-chrome-stable telegram vlc
-
-#printf "${yellow}>>>${nc} ${cyan}Applying theme...${nc}\n"
 
 printf "${yellow}>>>${nc} ${cyan}Installing fonts...${nc}\n"
 git clone https://github.com/powerline/fonts.git $HOME/Downloads/PowerlineFonts
