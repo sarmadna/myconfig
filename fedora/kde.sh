300a219cca205c8ad7deee67abc1f1de919cf88a
Sarmad <sarmad_n_a@hotmail.com>
2022-05-19 20:58:43 +0300

    Update kde.sh

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -20,7 +20,9 @@ neofetch \
 cava \
 fedora-workstation-repositories \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+mediawriter \
+sqlite
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf -y install google-chrome-stable telegram vlc
 
