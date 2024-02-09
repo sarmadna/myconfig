ac1b430ea2e1c5813f778a84021587159dd6c92d
sarmadna <sarmad_n_a@hotmail.com>
2024-02-09 18:33:57 +0300

    update

--- dotfiles/macmon.sh
+++ dotfiles/macmon.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-tmux new-session 'tty-clock -Bcst -C 0' \; split-window -h 'cava' \; split-window -v 'cmatrix -a -b -C black -u 2' \; select-pane -t 0 \; split-window -v 'caffeinate -d' \;
+tmux new-session 'tty-clock -Bcst -C 0' \; \
+	split-window -h 'cava' \; \
+	split-window -v 'cmatrix -a -b -C black -u 2' \; \
+	select-pane -t 0 \; \
+	split-window -v 'caffeinate -d' \;
 
