d008ed0ac2c9c9b0a4896d02a52a1e7129d8cb34
sarmadna <sarmad_n_a@hotmail.com>
2024-03-23 21:59:32 +0300

    update

---
+++ debian/base.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+yellow='\033[1;33m'
+cyan='\033[1;36m'
+red='\033[1;31m'
+nc='\033[0m'
+
+printf "${yellow}>>>${nc} ${cyan}Configuring the system...${nc}\n"
+sudo timedatectl set-timezone Asia/Baghdad
+
+printf "${yellow}>>>${nc} ${cyan}Updating the system...${nc}\n"
+sudo apt update && sudo apt upgrade -y
+
+printf "${yellow}>>>${nc} ${cyan}Installing cli apps...${nc}\n"
+sudo apt -y install \
+	vim \
+	zsh \
+	wget \
+	curl \
+	htop \
+	sqlite3 \
+	cmatrix \
+	neofetch \
+	btrfs-progs \
+    fonts-terminus \
+    fonts-terminus-otb
+
+printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp ../dotfiles/vimrc $HOME/.vimrc
+vim +PluginInstall +qall
+
+printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ../dotfiles/zshrc $HOME/.zshrc
+chsh -s $(which zsh)
+
+printf "${yellow}>>>${nc} ${red}Done...!${nc}\n"
