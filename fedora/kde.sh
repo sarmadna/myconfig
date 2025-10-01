9fbfbd9e9102d177940c0edbdee19c92d9142670
sarmadna <sarmad_n_a@hotmail.com>
2025-10-01 09:43:05 +0300

    update

--- fedora/kde.sh
+++ fedora/kde.sh
@@ -5,15 +5,29 @@ cyan='\033[1;36m'
 nc='\033[0m'
 
 printf "${yellow}>>>${nc} ${cyan}Installing apps...${nc}\n"
-sudo dnf -y install \
-	plasma-desktop \
-	konsole \
-	dolphin \
-	discover \
-	kscreen \
-	plasma-nm \
-	firefox \
-	gnome-abrt \
+sudo dnf install -y \
+	gnome-disk-utility \
 	papirus-icon-theme
+
+printf "${yellow}>>>${nc} ${cyan}Removing some useless apps...${nc}\n"
+sudo dnf remove \
+	akregator \
+	kmahjongg \
+	kmines \
+	kpat \
+	kaddressbook \
+	kmail \
+	dragon \
+	elisa-player \
+	korganizer \
+	kamoso \
+	qrca \
+	kolourpaint \
+	skanpage \
+	neochat \
+	kde-connect \
+	krdc \
+	krfb 
+
 
 printf "${yellow}>>>${nc} ${red}Done...!${nc}\n"
