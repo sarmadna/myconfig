a2e5ba6c91e5c7431f1a77cf702e64c4c1f42f01
sarmadna <sarmad_n_a@hotmail.com>
2025-10-31 20:05:37 +0300

    update

--- fedora/base.sh
+++ fedora/base.sh
@@ -18,6 +18,7 @@ sudo dnf -y upgrade --refresh
 
 printf "${yellow}>>>${nc} ${cyan}Installing cli apps...${nc}\n"
 sudo dnf -y install \
+	git \
 	vim \
 	zsh \
 	wget \
@@ -31,24 +32,23 @@ sudo dnf -y install \
 	terminus-fonts-console
 
 printf "${yellow}>>>${nc} ${cyan}Installing fonts...${nc}\n"
-# Terminus font for console
 sudo mv /etc/vconsole.conf /etc/vconsole.conf.bak
-sudo cp ../dotfiles/vconsole.conf /etc/vconsole.conf
-# Nerd font for powerlevel10k
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P $HOME
-sudo mv $HOME/*.ttf /usr/share/fonts
-# Some other fonts
-sudo dnf -y install pt-sans-fonts google-noto-kufi-arabic-fonts google-noto-sans-arabic-fonts
+#sudo cp ../dotfiles/vconsole.conf /etc/vconsole.conf
+sudo wget https://sarmadna.github.io/myconfig/dotfiles/vconsole.conf -P /etc/
 
 printf "${yellow}>>>${nc} ${cyan}Setting up Vim...${nc}\n"
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-cp ../dotfiles/vimrc $HOME/.vimrc
+#cp ../dotfiles/vimrc $HOME/.vimrc
+wget https://sarmadna.github.io/myconfig/dotfiles/vimrc -P $HOME
+mv $HOME/vimrc $HOME/.vimrc
 vim +PluginInstall +qall
 
 printf "${yellow}>>>${nc} ${cyan}Installing zsh and oh-my-zsh...${nc}\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-cp ../dotfiles/zshrc $HOME/.zshrc
+#cp ../dotfiles/zshrc $HOME/.zshrc
+wget https://sarmadna.github.io/myconfig/dotfiles/zshrc -P $HOME
+mv $HOME/zshrc $HOME/.zshrc
 chsh -s $(which zsh)
 
 printf "${yellow}>>>${nc} ${red}Done...!${nc}\n"
--- fedora/kde.sh
+++ fedora/kde.sh
@@ -10,8 +10,15 @@ sudo dnf install -y \
 	gnome-disk-utility \
 	papirus-icon-theme
 
+printf "${yellow}>>>${nc} ${cyan}Installing fonts...${nc}\n"
+# Nerd font for powerlevel10k
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P $HOME
+sudo mv $HOME/*.ttf /usr/share/fonts
+# Some RTL fonts
+sudo dnf -y install pt-sans-fonts google-noto-kufi-arabic-fonts google-noto-sans-arabic-fonts
+
 printf "${yellow}>>>${nc} ${cyan}Removing some useless apps...${nc}\n"
-sudo dnf remove \
+sudo dnf remove -y \
 	akregator \
 	kmahjongg \
 	kmines \
