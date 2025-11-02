9b845b0aaf9f5189e2b10283c82147302799cc2d
sarmadna <sarmad_n_a@hotmail.com>
2025-11-02 11:33:28 +0300

    update

--- fedora/dnfconf.sh
+++ fedora/dnfconf.sh
@@ -1,11 +1,6 @@
 #! /bin/sh
 
-#This shell script configures the dnf.conf file
-#and installs gdisk in fedora live boot media.
-
 echo "Adding the following lines to dnf.conf file"
 sudo echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
 sudo echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
 sudo echo "deltarpm=True" | sudo tee -a /etc/dnf/dnf.conf
-
-sudo dnf install gdisk -y
